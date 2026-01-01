cf2e0f8934e7021abfd8fcebff6bb80a38d02d87
Salma <salmaabdullahi999@gmail.com>
2026-01-01 19:04:34 +0100

    server build ci fixed

--- server/tests/Startup.cs
+++ server/tests/Startup.cs
@@ -13,12 +13,19 @@ namespace tests;
 
 public class Startup
 {
+    private static readonly PostgreSqlContainer DbContainer = new PostgreSqlBuilder().Build();
+    private static readonly object InitLock = new();
+    private static bool _initialized;
+    private static bool _containerStarted;
     public static void ConfigureServices(IServiceCollection services)
     {
         Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
-        var postgreSqlContainer = new PostgreSqlBuilder().Build();
-        postgreSqlContainer.StartAsync().GetAwaiter().GetResult();
-        var connectionString = postgreSqlContainer.GetConnectionString();
+        if (!_containerStarted)
+        {
+            DbContainer.StartAsync().GetAwaiter().GetResult();
+            _containerStarted = true;
+        }
+        var connectionString = DbContainer.GetConnectionString();
         var configuration = new ConfigurationBuilder()
             .AddInMemoryCollection(new Dictionary<string, string?>
             {
@@ -28,19 +35,38 @@ public class Startup
         services.AddSingleton<IConfiguration>(configuration);
         Program.ConfigureServices(services, configuration);
         services.RemoveAll(typeof(MyDbContext));
-        services.AddScoped<MyDbContext>(factory =>
+        services.AddDbContext<MyDbContext>(options =>
         {
-            var options = new DbContextOptionsBuilder<MyDbContext>()
-                .UseNpgsql(connectionString)
-                .Options;
-
-            var ctx = new MyDbContext(options);
-            ctx.Database.EnsureCreated();
-            return ctx;
+            options.UseNpgsql(connectionString);
         });
+        
+        EnsureDatabaseCreated(services, connectionString);
         services.RemoveAll<TimeProvider>();
         var fakeTime = new FakeTimeProvider();
         services.AddSingleton<TimeProvider>(fakeTime);
         services.AddScoped<ISeeder, Seeder>();
+    }
+    
+    private static void EnsureDatabaseCreated(IServiceCollection services, string connectionString)
+    {
+        if (_initialized)
+        {
+            return;
+        }
+
+        lock (InitLock)
+        {
+            if (_initialized)
+            {
+                return;
+            }
+
+            var options = new DbContextOptionsBuilder<MyDbContext>()
+                .UseNpgsql(connectionString)
+                .Options;
+            using var ctx = new MyDbContext(options);
+            ctx.Database.EnsureCreated();
+            _initialized = true;
+        }
     }
 }
